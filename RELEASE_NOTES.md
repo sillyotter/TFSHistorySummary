ad589bc921dd8f7e30d2dadb1d8406101826727c
Guy Oliver <guy.oliver@gmail.com>
2015-02-15 10:15:00 -0500

    adjusted fake to build just what's needed, dumped extra stuff

---
+++ RELEASE_NOTES.md
@@ -0,0 +1,2 @@
+#### 0.0.1-beta - February 15, 2015
+* Initial release
--- src/TFSHistorySummary/AssemblyInfo.fs
+++ src/TFSHistorySummary/AssemblyInfo.fs
@@ -4,9 +4,9 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("TFSHistorySummary")>]
 [<assembly: AssemblyProductAttribute("TFSHistorySummary")>]
 [<assembly: AssemblyDescriptionAttribute("Scans TFS history dump to extract statistics")>]
-[<assembly: AssemblyVersionAttribute("1.0")>]
-[<assembly: AssemblyFileVersionAttribute("1.0")>]
+[<assembly: AssemblyVersionAttribute("0.0.1")>]
+[<assembly: AssemblyFileVersionAttribute("0.0.1")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "1.0"
+    let [<Literal>] Version = "0.0.1"
