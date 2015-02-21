22aa38f329777eb0f167c993f02f206dd279aae3
Guy Oliver <guy.oliver@gmail.com>
2015-02-20 19:39:50 -0500

    Rework for the fun of it.

--- src/TFSHistorySummary/TFSHistorySummary.fs
+++ src/TFSHistorySummary/TFSHistorySummary.fs
@@ -10,7 +10,7 @@ type ParserState =
     | LookingForItem
 
 type Change = 
-    { changeType : string list
+    { changeType : string
       filePath : string }
 
 type ChangeSetInfo = 
@@ -32,68 +32,69 @@ let (|Regex|_|) (regex : Regex) (str : string) =
         Some(m.Groups
              |> Seq.cast<Group>
              |> Seq.skip 1
-             |> Seq.map (fun x -> 
-                    x.Captures
-                    |> Seq.cast<Capture>
-                    |> Seq.map (fun x -> x.Value)
-                    |> Seq.toList)
+             |> Seq.map (fun x -> x.Value)
              |> Seq.toList)
     else None
 
-let validNames = ["Fankhauser"; "Hunt"; "Vance"; "Oliver"; "Bennett"]
-let userRegex = Regex(@"^User:\s+(.+)$", RegexOptions.Compiled|||RegexOptions.Singleline);
-let editRegex = Regex(@"^\s+(?:([a-z ]+)(?:[, ]+)?)+\s+\$(.+)", RegexOptions.Compiled|||RegexOptions.Singleline)
+let validNames = [ "Fankhauser"; "Hunt"; "Vance"; "Oliver"; "Bennett" ]
+let userRegex = Regex(@"^User:\s+(.+)$", RegexOptions.Compiled ||| RegexOptions.Singleline)
+let editRegex = Regex(@"^\s+(.+)\s+\$(.+)", RegexOptions.Compiled ||| RegexOptions.Singleline)
+let updateState s pd = { pd with state = s }
+let updateUser u pd = { pd with current = { pd.current with user = u } }
 
-let statemachine (s : ParserData) (l : string) = 
-    match s.state with
+let resetCurrent pd = 
+    { pd with state = ParserState.LookingForStartOfRecord
+              current = 
+                  { user = ""
+                    items = List.empty } }
+
+let appendItems i pd = { pd with current = { pd.current with items = i :: pd.current.items } }
+
+let pushCurrent pd = 
+    if pd.current.user <> "" && pd.current.items.Length > 0 then 
+        { pd with state = ParserState.LookingForStartOfRecord
+                  changes = pd.current :: pd.changes }
+        |> resetCurrent
+    else resetCurrent pd
+
+let doStep (l : string) = 
+    function 
     | ParserState.LookingForStartOfRecord -> 
         match l with
-        | StartsWith @"---" -> { s with state = ParserState.LookingForUser }
-        | _ -> s
+        | StartsWith @"---" -> updateState ParserState.LookingForUser
+        | _ -> id
     | ParserState.LookingForUser -> 
         match l with
-        | Regex userRegex [ [ u ] ] -> 
-            if validNames |> Seq.map (fun x -> u.Contains(x)) |> Seq.exists (fun x -> x) then
-                { s with state = ParserState.LookingForItemsGroup
-                         current = { s.current with user = u } }
-            else
-                { s with state = ParserState.LookingForStartOfRecord
-                         current = 
-                             { user = ""
-                               items = List.empty }  }
-        | _ -> s
+        | Regex userRegex [ u ] -> 
+            if validNames
+               |> Seq.map (fun x -> u.Contains(x))
+               |> Seq.exists (fun x -> x)
+            then updateState ParserState.LookingForItemsGroup >> updateUser u
+            else updateState ParserState.LookingForStartOfRecord >> resetCurrent
+        | _ -> id
     | ParserState.LookingForItemsGroup -> 
         match l with
-        | StartsWith @"Items:" -> { s with state = ParserState.LookingForItem }
-        | _ -> s
+        | StartsWith @"Items:" -> updateState ParserState.LookingForItem
+        | _ -> id
     | ParserState.LookingForItem -> 
         match l with
-        | Regex editRegex [ edits; [ path ] ] -> 
-            let cleanEdits = edits |> List.map (fun x -> x.Trim())
-            if List.exists (fun x -> x = "add" || x = "edit") cleanEdits && not(path.Contains("-Release")) then 
-                { s with current = 
-                             { s.current with items = 
-                                                  { changeType = cleanEdits
-                                                    filePath = path }
-                                                  :: s.current.items } }
-            else
-                s
-        | _ -> 
-            if s.current.user <> "" && s.current.items.Length > 0 then
-                { s with state = ParserState.LookingForStartOfRecord
-                         changes = s.current :: s.changes
-                         current = 
-                             { user = ""
-                               items = List.empty } }
-            else
-                { s with state = ParserState.LookingForStartOfRecord
-                         current = 
-                             { user = ""
-                               items = List.empty } }
+        | Regex editRegex [ edits; path ] -> 
+            let isOk = 
+                edits.Split(',')
+                |> Array.map (fun x -> x.Trim())
+                |> Array.exists (fun x -> x = "add" || x = "edit")
+            
+            let notRelease = not (path.Contains("-Release"))
+            if isOk && notRelease then 
+                appendItems { changeType = edits
+                              filePath = path }
+            else id
+        | _ -> pushCurrent
 
+let stateMachine (s : ParserData) (l : string) = doStep l s.state s
 
 [<EntryPoint>]
-let main args =
+let main args = 
     let initialState = 
         { state = ParserState.LookingForStartOfRecord
           changes = List.empty
@@ -102,22 +103,14 @@ let main args =
                 items = List.empty } }
     File.ReadLines args.[0]
     |> Seq.map (fun x -> x.TrimEnd())
-    |> Seq.fold statemachine initialState
-    |> fun s -> 
-        if s.current.user <> "" then
-            { s with state = ParserState.LookingForStartOfRecord
-                     changes = s.current :: s.changes
-                     current = 
-                         { user = ""
-                           items = List.empty } }
-        else
-            s
+    |> Seq.fold stateMachine initialState
     |> fun s -> s.changes
     |> Seq.groupBy (fun x -> x.user)
     |> Seq.map (fun (k, x) -> 
-           k, 
-           x |> Seq.length, 
-           x |> Seq.collect (fun y -> y.items) |> Seq.length)
+           k, x |> Seq.length, 
+           x
+           |> Seq.collect (fun y -> y.items)
+           |> Seq.length)
     |> Seq.sortBy (fun (_, c, _) -> -c)
     |> Seq.iter (fun (name, commits, edits) -> printfn "%s\t%d\t%d" name commits edits)
     0
